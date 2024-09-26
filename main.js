b6bdbc349e1ed76c8a1281c519541c5568ec9aaa
Khatymiss <112128264+Khatymiss707@users.noreply.github.com>
2024-09-26 17:59:43 -0400

    cdn_better

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 import "../federation-des-locataires--web-chibis/styles.scss";
-import Swiper from './node_modules/swiper/swiper-bundle';
-import './node_modules/swiper/css';
 
 const swiper = new Swiper('.swiper', {
     loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+    centeredSlides: true, 
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+      },
   });
