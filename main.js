f3f7bc65f0197d84214054a524f10e5ba5fbde84
Sitmonternna <2163597@cmontmorency.qc.ca>
2024-09-30 02:20:19 -0400

    nav_almostdone

--- main.js
+++ main.js
@@ -8,3 +8,11 @@ const swiper = new Swiper('.swiper', {
         disableOnInteraction: false,
       },
   });
+
+  //burger toggle
+    var links = document.querySelectorAll(".nav__list--dropdown");
+  let burger = document.querySelector('.burger');
+  burger.addEventListener('click', function() {
+    links.classList.toggle('visible');
+  });
+
