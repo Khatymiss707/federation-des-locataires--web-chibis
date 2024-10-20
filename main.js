d2e5db71e6b286fd2ed4d4b712849a2b4d8666a6
Khatymiss <khalytia@hotmail.ca>
2024-10-19 22:59:42 -0400

    try_wp_02

---
+++ main.js
@@ -0,0 +1,48 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  centeredSlides: true,
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+const swiper02 = new Swiper('.swiper02', {
+  loop: true,
+  spaceBetween: 100,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+});
+
+  //burger toggle
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav_menu");
+
+  hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  })
+
+  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })); 
+
+  //accordeon
+let title = document.querySelector(".accordion__item__title"); 
+let contenu = document.querySelector(".accordion__item__content--open"); 
+
+var $title = $('.title');
+var content   = '.content';
+$title.click(function () {
+  $(this).next(content).slideToggle();
+  $(this).parent().siblings().children().next().slideUp();
+  return false;
+});
+
